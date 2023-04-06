96a762bdc7d2098d5682d48d514e0e708e435847
Alisochka1991 <alisiaagranov@gmail.com>
2023-04-06 18:37:19 +0300

    com2

--- src/main/java/schedulerscreens/BaseScreen.java
+++ src/main/java/schedulerscreens/BaseScreen.java
@@ -2,12 +2,26 @@ package schedulerscreens;
 
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
+import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import org.openqa.selenium.support.PageFactory;
 
 public class BaseScreen {
     AppiumDriver<MobileElement>driver;
 
     public BaseScreen(AppiumDriver<MobileElement> driver) {
         this.driver = driver;//sgenerirovany konstructor
+        PageFactory.initElements(new AppiumFieldDecorator(driver), this);
 
+    }
+    public void type(MobileElement element, String text)
+    {
+        if (text!=null) {
+            element.click();
+            element.clear();
+            element.sendKeys(text);
+        }
+    }
+    public void hideKeyboard(){
+        driver.hideKeyboard();
     }
 }
