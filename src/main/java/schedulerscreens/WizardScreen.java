1ea715679eee4583f4cebc73011b7434efc61d03
Alisochka1991 <alisiaagranov@gmail.com>
2023-04-09 09:06:21 +0300

    com3

--- src/main/java/schedulerscreens/WizardScreen.java
+++ src/main/java/schedulerscreens/WizardScreen.java
@@ -2,9 +2,12 @@ package schedulerscreens;
 
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
+import org.openqa.selenium.support.FindBy;
 
 public class WizardScreen extends BaseScreen {
     public WizardScreen(AppiumDriver<MobileElement> driver) {
         super(driver);
     }
+    @FindBy (xpath="//*[@resource-id='bla bla']")
+    MobileElement skipButton;
 }
